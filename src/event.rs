689582f138bf47577022743529adb9fdb6729aa5
Andrew Wason <rectalogic@rectalogic.com>
2025-08-22 18:55:44 -0400

    macro to create events

--- src/event.rs
+++ src/event.rs
@@ -2,29 +2,22 @@ use bevy::prelude::*;
 
 use crate::listener::EventType;
 
-#[derive(Event, Copy, Clone, Debug)]
-pub struct LoadedMetadata;
-impl EventType for LoadedMetadata {
-    const EVENT_NAME: &'static str = "loadedmetadata";
-    fn new() -> Self {
-        Self
-    }
+#[macro_export]
+macro_rules! new_event_type {
+    ($name:ident, $event_name:literal) => {
+        #[derive(Event, Copy, Clone, Debug)]
+        pub struct $name;
+
+        impl EventType for $name {
+            const EVENT_NAME: &'static str = $event_name;
+
+            fn new() -> Self {
+                Self
+            }
+        }
+    };
 }
 
-#[derive(Event, Copy, Clone, Debug)]
-pub struct Resize;
-impl EventType for Resize {
-    const EVENT_NAME: &'static str = "resize";
-    fn new() -> Self {
-        Self
-    }
-}
-
-#[derive(Event, Copy, Clone, Debug)]
-pub struct Playing;
-impl EventType for Playing {
-    const EVENT_NAME: &'static str = "playing";
-    fn new() -> Self {
-        Self
-    }
-}
+new_event_type!(LoadedMetadata, "loadedmetadata");
+new_event_type!(Resize, "resize");
+new_event_type!(Playing, "playing");
